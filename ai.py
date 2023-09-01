63d06571ba810982702ca442aec43960f9c4c5fa
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-01 13:57:06 -0700

    created Brain class for handling interactions with openai, moved converse function to Brain class

--- ai.py
+++ ai.py
@@ -11,3 +11,23 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 db = get_db()
+
+class Brain:
+    
+  def __init__(self, user):
+    self.user = user
+    self.model = 'gpt-3.5-turbo'
+    self.message_chain = []
+
+  def __str__(self):
+    return f'{self.user}'
+
+  # handles formatting of call to openai API and returns response
+  def converse(self, content, directive):
+    self.message_chain.append({ 'role': 'system', 'content': directive })
+    self.message_chain.append({ 'role': 'user', 'content': content })
+    response = openai.ChatCompletion.create(
+      model=self.model,
+      messages=self.message_chain
+    )['choices'][0]['message']['content']
+    return response
