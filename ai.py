2e192692c79060b04a4a21d597879ac553f51281
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-01 17:35:18 -0700

    messages from user and ai now record in db and can be recalled for context in future messages

--- ai.py
+++ ai.py
@@ -18,16 +18,32 @@ class Brain:
     self.user = user
     self.model = 'gpt-3.5-turbo'
     self.message_chain = []
+    self.collection = db[user]
 
   def __str__(self):
     return f'{self.user}'
+  
+  # records a message and adds to db
+  def record(self, message, role):
+    self.collection.insert_one({ 'role': role, 'content': message, 'timestamp': datetime.now() })
 
   # handles formatting of call to openai API and returns response
   def converse(self, content, directive):
+    self.record(content, 'user')
+    message_history = self.collection.find().sort('timestamp', -1)
     self.message_chain.append({ 'role': 'system', 'content': directive })
+
+    # for cost reasons, this loop breaks after 4 messages have been gathered
+    # it can be changed or eliminated altogether depending on the server's needs/resources
+    for idx, message in enumerate(message_history):
+      if idx > 4:
+        break
+      self.message_chain.append({ 'role': message['role'], 'content': message['content'] })
+
     self.message_chain.append({ 'role': 'user', 'content': content })
     response = openai.ChatCompletion.create(
       model=self.model,
       messages=self.message_chain
     )['choices'][0]['message']['content']
+    self.record(response, 'assistant')
     return response
